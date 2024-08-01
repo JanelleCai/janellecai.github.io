ecee5ca12aa9b830b9653e45c3f6c84826d20920
Janelle Cai <jcai18@mit.edu>
2024-07-31 22:16:36 -0700

    remove teaching page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,12 @@ function Navbar() {
 
   return (
     <nav className="px-8 py-6">
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container mx-auto flex justify-between items-center max-w-3xl">
         <div className="navbar-header">Janelle Cai</div>
         <ul className="flex space-x-4">
           <li><Link to="/" className={`hover:text-cyan-900 ${location.pathname === '/' ? 'text-cyan-900' : 'text-cyan-600'}`}>home</Link></li>
           <li><Link to="/projects" className={`hover:text-cyan-900 ${location.pathname === '/projects' ? 'text-cyan-900' : 'text-cyan-600'}`}>projects</Link></li>
-          <li><Link to="/teaching" className={`hover:text-cyan-900 ${location.pathname === '/teaching' ? 'text-cyan-900' : 'text-cyan-600'}`}>teaching</Link></li>
+          {/* <li><Link to="/teaching" className={`hover:text-cyan-900 ${location.pathname === '/teaching' ? 'text-cyan-900' : 'text-cyan-600'}`}>teaching</Link></li> */}
         </ul>
       </div>
     </nav>
